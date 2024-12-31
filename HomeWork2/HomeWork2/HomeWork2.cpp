6ab83e1cfe8b3e85df57bf2331b006551ecac275
VINagaev <15vlad19@gmail.com>
2024-12-31 20:42:38 +0700

    +

---
+++ HomeWork2/HomeWork2/HomeWork2.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	/*cout << "Wham!- Last Christmas \n\n";
+	cout << "Last Christmas, I gave you my heart \n";
+	cout << "But the very next day, you gave it away \n";
+	cout << "This year, to save me from tears \n";
+	cout << "I'll give it to someone special \n\n";
+
+	cout << "Last Christmas, I gave you my heart \n";
+	cout << "But the very next day, you gave it away(you gave it away) \n";
+	cout << "This year, to save me from tears \n";
+	cout << "I'll give it to someone special (special) \n";*/
+
+
+
+	/*cout << "Every" << "\n";
+	cout << "\tHunter" << "\n";
+	cout << "\t\tWants" << "\n";
+	cout << "\t\t\tTo know" << "\n";
+	cout << "\t\t\t\t Where" << "\n";
+	cout << "\t\t\t\t\tDoes Pheasant" << "\n";
+	cout << "\t\t\t\t\t\t\tSit" << "\n\n";*/
+
+
+
+	std::system("chcp 1251");
+	cout << "\t\t\t\t\tПродам ноутбук" << "\n";
+	cout << "\t\t\t\t\tФирмы: MSI" << "\n";
+	cout << "\t\t\t\t\tТел: 222-22-22" << "\n";
+	cout << "\t\t\t\t\t---------------" << "\n";
+	cout << "\t\t\t\t\t222\t|22-22|" << "\n";
+	cout << "\t\t\t\t\t222\t|22-22|" << "\n";
+	cout << "\t\t\t\t\t222\t|22-22|" << "\n";
+}
+
